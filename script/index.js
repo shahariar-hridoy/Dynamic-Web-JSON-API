34b98028274eda96e6a2b821742880235a71d825
shahariar-hridoy <shahariark79@gmail.com>
2025-03-13 03:09:52 +0600

    dynamic name

---
+++ script/index.js
@@ -0,0 +1,20 @@
+const loadUser = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+        showUser(data);
+    })
+}
+
+
+const showUser = (users) =>{
+
+    const userContainer = document.getElementById('users');
+
+
+    for(const user of users){
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        userContainer.appendChild(li);
+    }
+}
